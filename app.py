8e827005e388be67b4f154e47ad3950488f00a5b
mohammadrezahey <37912168+mohammadrezahey@users.noreply.github.com>
2023-02-14 19:43:19 +0330

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     parser.add_argument('--port', help='Port to bind to.',
                         default=443)
     parser.add_argument('--remote_url', help='Remote websocket url',
-                        default='wss://d2c73by1qw1ztu.cloudfront.net:443')
+                        default='wss://mars.mohammadreza.space:443')
     args = parser.parse_args()
 
     REMOTE_URL = args.remote_url
